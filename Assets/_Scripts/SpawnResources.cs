4a9318fd5c4cd3690ecf5722a82025a06d86796a
Vincent-207 <143017105+Vincent-207@users.noreply.github.com>
2026-02-06 10:48:58 -0600

    Resource spawning

---
+++ Assets/_Scripts/SpawnResources.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnResources : MonoBehaviour
+{
+    [SerializeField] GameObject resourcePrefab, resourceHolder;
+    public int xSize, zSize;
+    public float xScale, yScale, spawnScale, spawnThreshold, spawnChance;
+    public bool Regen = false;
+    void Start()
+    {
+        GenerateNodes();
+    }
+    
+    void Update()
+    {
+        if(Regen)
+        {
+            DestroyAllChildren();
+            GenerateNodes();
+            Regen = false;
+        }
+    }
+
+    void DestroyAllChildren()
+    {
+        List<GameObject> children = new();
+        for(int childIndex = 0; childIndex < resourceHolder.transform.childCount; childIndex++)
+        {
+            children.Add(resourceHolder.transform.GetChild(childIndex).gameObject);
+        }
+
+        foreach(GameObject child in children)
+        {
+            Destroy(child);
+        }
+    }
+
+    void GenerateNodes()
+    {
+        for(int z = 0; z <= zSize; z++)
+        {
+            for(int x = 0; x <= xSize; x++)
+            {   
+                float perlinValue = yScale * Mathf.PerlinNoise(x * spawnScale , z * spawnScale);
+                if(spawnThreshold >= perlinValue)
+                {
+                    float chance = Random.Range(0f, 1f);
+                    if(spawnChance > chance) CreateNode(x, z);
+                }
+            }
+            
+        }
+    }
+
+    void CreateNode(int x, int z)
+    {
+        Vector3 spawnPos = new Vector3(x * xScale,0, z * xScale);
+        Instantiate(resourcePrefab, spawnPos, Quaternion.identity, resourceHolder.transform);
+
+    }
+}
