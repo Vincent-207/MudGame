36670865ae79f305ee647ab6492a0fbda96d6108
Vincent-207 <vhannah2007@outlook.com>
2026-03-22 19:00:56 -0500

    Started craeture spawning

---
+++ Assets/_Scripts/CreatureSpawning.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using UnityEngine;
+
+public class CreatureSpawning : MonoBehaviour
+{
+    GameObject CreaturePrefabs;
+
+    public void DoWaves(int creaturesPerWave, float duration)
+    {
+        
+    }
+
+    IEnumerator DoWave(float duration, float thisWaveTime, int creatureSpawnCount)
+    {
+        float spawnRadius = 15f;
+        for(int creatureIndex = 0; creatureIndex < creatureSpawnCount; creatureIndex++);
+        
+    }
+}
